10e478359538779d74ce4917303b82d990cedc4e
Ming Duan <11203duan@gmail.com>
2019-05-13 18:21:14 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -3,7 +3,7 @@ class CreateSongs < ActiveRecord::Migration[4.2]
   def change
     create_table :songs do |t|
       t.string :name
-      t.integer :artist_id 
+      t.integer :artist_id
       t.integer :genre_id
     end
   end
